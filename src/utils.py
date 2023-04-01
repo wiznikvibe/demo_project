3feb2acdef531addec658701c4804fc1155282e5
Nikhil Shetty <nikhilshetty439@gmail.com>
2023-04-01 08:33:28 +0530

    Model Training done

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 import pandas as pd
 import numpy as np
 import dill
@@ -15,3 +15,25 @@ def save_object(file_path,obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report 
+    except Exception as e:
+        raise CustomException(e, sys)
